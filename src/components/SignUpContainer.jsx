45407d7bff28b8e567f58639eaba6119832261aa
bharath <bharathrnair7@gmail.com>
2022-05-12 11:40:27 +0000

    loginpage-backend-frontend-api

--- src/components/SignUpContainer.jsx
+++ src/components/SignUpContainer.jsx
@@ -1,47 +1,120 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 const SignUpContainer = () => {
+
+    // function _(id){
+    //     return document.getElementById(id);
+    // }
+
+    // function main()
+    // {
+    //     _("signup-form").addEventListener("submit",UserSignUp);
+
+    // }
+
+    const [values, setValues] = useState({firstName: "", lastName: "",
+    Email:"",DateOfBirth:"",PhoneNumber:"",State:"",City:""})
+    
+    const {firstName,lastName,Email,DateOfBirth,PhoneNumber,State,City} = values;
+
+    const onChange = (key,value)=>{
+        setValues(prev =>({
+            ...prev,
+            [key]:value
+        }))
+    }
+
+    const formSubmit = (e)=>{
+        e.preventDefault();
+     fetch("https://localhost:44371/api/users",{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+          },
+        body:JSON.stringify({...values,dob:DateOfBirth})
+     }).then(res => res.json())
+     .then((res)=>{
+         console.log(res)
+     });
+    }
+
   return (
+
     <div className= "signUpContainer">
+        <form onSubmit={formSubmit}>
     <div className="signUpSection">
     <p id='two'>Sign Up</p>
     <div className="signUpInput">
-    <input type="text" placeholder='First Name' id='firstName' />
-    <input type="text" placeholder='Last Name' id='lastName' />
-    </div>
-    <div className="formField">
-      <p>E-mail</p>
-    <input type="text" />
-    </div>
-    <div className="formField">
-      <p>Create-Password</p>
-    <input type="password" />
-    </div>
-    <div className="formField">
-      <p>Confirm-Password</p>
-    <input type="password" />
-    </div>
-    <div className="formField">
-      <p>Date of Birth</p>
-    <input type="date" />
-    </div>
-    <div className="formField">
-      <p>Phone Number</p>
-    <input type="text" />
-    </div>
-    <div className="formField">
-      <p>State</p>
-    <input type="text" />
-    </div>
-    <div className="formField">
-      <p>City</p>
-    <input type="text" />
-    </div>
-    <button id='signUpButton'>Sign-up</button>
+
+    <input type="text" placeholder='First Name' id='firstName' 
+    value={firstName}
+    onChange={(e) => onChange("firstName",e.target.value)} />
+
+
+    <input type="text" placeholder='Last Name' id='lastName'
+    value={lastName} 
+    onChange={(e) => onChange("lastName",e.target.value)}/>
     </div>
 
+    <div className="formField">
+      <p>E-mail</p>
+    <input type="text" 
+    value={Email} 
+    onChange={(e) => onChange("Email",e.target.value)} />
+    </div>
+
+    <div className="formField">
+      <p>Create-Password</p>
+    <input type="password"
+    //  value={Password} 
+    //  onChange={(e) => onchange("Password",e.target.value)}
+      /> 
+    </div>
+
+    <div className="formField">
+      <p>Confirm-Password</p>
+    <input type="password" 
+    //  value={ConfirmPassword} 
+    //  onChange={(e) => onchange("ConfirmPassword",e.target.value)} 
+     />
+    </div>
+
+    <div className="formField">
+      <p>Date of Birth</p>
+    <input type="date"
+     value={DateOfBirth} 
+     onChange={(e) => onChange("DateOfBirth",e.target.value)} /> 
+    </div>
+
+    <div className="formField">
+      <p>Phone Number</p>
+    <input type="text"
+     value={PhoneNumber} 
+     onChange={(e) => onChange("PhoneNumber",e.target.value)} /> 
+    </div>
+
+    <div className="formField">
+      <p>State</p>
+    <input type="text" 
+     value={State} 
+     onChange={(e) => onChange("State",e.target.value)} />
+    </div>
+
+    <div className="formField">
+      <p>City</p>
+    <input type="text"
+     value={City} 
+     onChange={(e) => onChange("City",e.target.value)} /> 
+    </div>
+
+    <button id='signUpButton'>Sign-up</button>
+    </div>
+    </form>
   </div>
   )
+
+
 }
 
 export default SignUpContainer
